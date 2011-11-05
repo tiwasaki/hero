fa2f6a7f0a7f3a79ecb692183c697c5ff3d3bf5b
unknown <taichiiwasaki@tt-a1449be12b78.(none)>
2011-11-05 17:25:34 +0900

    画面遷移の作成

--- app/controllers/top_controller.php
+++ app/controllers/top_controller.php
@@ -19,7 +19,10 @@ class TopController extends AppController {
 	function index() {
 		$url = GREE_PEOPLE_API_URL.'@me/@self';
 		$responseArray = json_decode($this->Oauth->requestAPI($url),true);
-		$this->Session->write('greeUserId', $responseArray['entry']['id']);
+		$greeUserId = $responseArray['entry']['id'];
+		if(!empty($greeUserId)){
+			$this->Session->write('greeUserId', $greeUserId);
+		}
 	}
 }
 ?>
